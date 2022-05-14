5752e34b2d8ce6eca1be240e2cae9f4bb1199857
Will <will@Wills.local>
2022-05-14 18:26:03 +0200

    first commit

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import WorkImg from "../assets/workImg.jpeg"
-import RealEstate from "../assets/realestate.jpg"
+import Drum from "../assets/drum-kit.jpg"
+import Rating from "../assets/rating.jpg"
 
 const Work = () => {
   return (
@@ -16,7 +16,52 @@ const Work = () => {
 
                 {/* Grid Item */}
                 <div 
-                    style={{backgroundImage: `url(${WorkImg})`}} 
+                    style={{backgroundImage: `url(${Drum})`}} 
+                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                React JS Application
+                            </span>
+
+                            <div className='pt-8 text-center'>
+                                <a href='https://doum-doum.netlify.app/' target="_blank" rel="noreferrer noopener">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                        Demo
+                                    </button>
+                                </a>
+                                <a href='https://github.com/Willistic/Drum-Kit' target="_blank" rel="noreferrer noopener">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                        Code
+                                    </button>
+                                </a>
+                            </div>
+                    </div>
+                </div>
+                <div 
+                    style={{backgroundImage: `url(${Rating})`}} 
+                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                React JS Application
+                            </span>
+
+                            <div className='pt-8 text-center'>
+                                <a href='https://react-rating.netlify.app/' target="_blank" rel="noreferrer noopener">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                </a>
+                                <a href='https://github.com/Willistic/React-Rating-Component' target="_blank" rel="noreferrer noopener">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                </a>
+                            </div>
+                    </div>
+                </div>
+                {/* Grid Item */}
+                <div 
+                    style={{backgroundImage: `url(${Drum})`}} 
                     className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover Effects */}
@@ -40,7 +85,7 @@ const Work = () => {
                     </div>
                 </div>
                 <div 
-                    style={{backgroundImage: `url(${RealEstate})`}} 
+                    style={{backgroundImage: `url(${Rating})`}} 
                     className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover Effects */}
@@ -61,7 +106,7 @@ const Work = () => {
                 </div>
                 {/* Grid Item */}
                 <div 
-                    style={{backgroundImage: `url(${WorkImg})`}} 
+                    style={{backgroundImage: `url(${Drum})`}} 
                     className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover Effects */}
@@ -85,52 +130,7 @@ const Work = () => {
                     </div>
                 </div>
                 <div 
-                    style={{backgroundImage: `url(${RealEstate})`}} 
-                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                React JS Application
-                            </span>
-
-                            <div className='pt-8 text-center'>
-                                <a href='/'>
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                                </a>
-                                <a href='/'>
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                                </a>
-                            </div>
-                    </div>
-                </div>
-                {/* Grid Item */}
-                <div 
-                    style={{backgroundImage: `url(${WorkImg})`}} 
-                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                React JS Application
-                            </span>
-
-                            <div className='pt-8 text-center'>
-                                <a href='/'>
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Demo
-                                    </button>
-                                </a>
-                                <a href='/'>
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Code
-                                    </button>
-                                </a>
-                            </div>
-                    </div>
-                </div>
-                <div 
-                    style={{backgroundImage: `url(${RealEstate})`}} 
+                    style={{backgroundImage: `url(${Rating})`}} 
                     className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover Effects */}
