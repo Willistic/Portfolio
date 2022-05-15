fde299ed6bfaa59d578c54a03af8d72f4631d07e
Will <will@Wills.local>
2022-05-15 15:44:50 +0200

    added photo

--- src/components/Work.js
+++ src/components/Work.js
@@ -72,7 +72,7 @@ const Work = () => {
                             </span>
 
                             <div className='pt-8 text-center'>
-                                <a href='https://integrify-first-assignment.netlify.app/'>
+                                <a href='https://integrify-first-assignment.netlify.app/' target="_blank" rel="noreferrer noopener">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                                         Demo
                                     </button>
